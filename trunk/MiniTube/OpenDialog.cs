93f1cde4e306ea35cb0d78c7bbcef4c29e526bfb
alishah.novin@gmail.com <alishah.novin@gmail.com@cb69aa31-31a9-9d56-f7b4-4f1bf7ebc362>
2011-09-02 16:50:48 +0000

    File upload

---
+++ trunk/MiniTube/OpenDialog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace MiniTube
+{
+    public partial class OpenDialog : Form
+    {
+        public OpenDialog()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+        }
+
+        private Regex _YoutubeRegex = new Regex(@"youtu(?:\.be|be\.com)/(?:.*v(?:/|=)|(?:.*/)?)([a-zA-Z0-9-_]+)");
+
+        private void GoBtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(UrlTxtBox.Text))
+            {
+                string url = UrlTxtBox.Text;
+                Match youtubeMatch = _YoutubeRegex.Match(url);
+                if (youtubeMatch.Success)
+                {
+                    MainWindow.Current.Open(youtubeMatch.Groups[1].Value);
+                }
+            }
+            this.Close();
+        }
+    }
+}
